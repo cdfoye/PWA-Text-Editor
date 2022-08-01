2528f9984597b7de6f98dda3a870e070554ec93b
cdfoye <cdfoye@gmail.com>
2022-08-01 15:28:39 -0600

    debugging console erros

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -20,7 +20,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore("jate");
   const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log("🚀 - data saved to the database", result);
+  // console.log("🚀 - data saved to the database", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -29,10 +29,10 @@ export const getDb = async () => {
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
-  console.log("result.value", result);
-  return result;
+  // console.log("result.value", result);
+  return result?.value;
 };
 
 initdb();
